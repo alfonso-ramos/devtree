0e2b0c86ea8982f26bd87eba086b235c59f453b3
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2025-09-16 17:01:25 -0700

    [ADD] Configuracion de cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
-import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
 import router from './router';
 import { connectDB } from './config/db';
+import { corsConfig } from './config/cors';
+
+connectDB();
 
 const app = express();
-connectDB();
+
+// Cors
+app.use(cors(corsConfig));
 
 // Read data form JSON bodys (forms, json, etc)
 app.use(express.json());
